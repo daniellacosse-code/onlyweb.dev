fa1a931fe4a4112d0a4f4bf00b8dbb55f056395f
Daniel LaCosse <D@nielLaCos.se>
2020-03-21 15:46:42 -0400

    routing!

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,53 @@
 <template>
-  <main>
-    <b-tabs v-model="activeTab" :animated="false" destroy-on-hide>
-      <b-tab-item label="Hello, World!">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
-      </b-tab-item>
-      <b-tab-item label="Other tab.">
-        Nothing to see here.
-      </b-tab-item>
-    </b-tabs>
-  </main>
+  <div id="app">
+    <b-navbar fixed-top>
+      <template slot="start">
+        <b-navbar-item to="/" tag="router-link">
+          Home
+        </b-navbar-item>
+        <b-navbar-item to="/about" tag="router-link">
+          About
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+    <router-view />
+  </div>
 </template>
 
 <script>
 import Vue from "vue";
-import VueRouter from "vue-router";
-import { Tabs } from "buefy";
-import * as pages from "./pages";
+import { Navbar } from "buefy";
 
-Vue.use(Tabs);
+Vue.use(Navbar);
 
-export default Vue.extend({
-  name: "App",
-  data: function() {
-    return {
-      activeTab: 0
-    };
-  },
+export default {
   components: {
-    HelloWorld
+    Navbar
   }
-});
+};
 </script>
 
 <style lang="scss">
 @import "~bulma";
-@import "~buefy/src/scss/components/_tabs";
 
-html,
-body,
-main {
-  width: 100vw;
-  height: 100vh;
-  font-family: HelveticaNeue, Helvetica, sans-serif;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
