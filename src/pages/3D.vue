f1b75a0cc03eeecd91e551e77168ec2b324f8a77
Daniel LaCosse <D@nielLaCos.se>
2020-03-28 18:30:59 -0400

    #D demo

---
+++ src/pages/3D.vue
@@ -0,0 +1,68 @@
+<template>
+  <canvas ref="canvas" />
+</template>
+
+<script>
+import {
+  Application,
+  Color,
+  Entity,
+  RESOLUTION_AUTO,
+  FILLMODE_FILL_WINDOW
+} from "playcanvas";
+
+export default {
+  methods: {
+    init() {
+      const canvas = this.$refs.canvas;
+      const app = new Application(canvas, {});
+
+      app.setCanvasFillMode(FILLMODE_FILL_WINDOW);
+      app.setCanvasResolution(RESOLUTION_AUTO);
+
+      window.addEventListener("resize", () => app.resizeCanvas());
+
+      // setup initial camera
+      const camera = new Entity("camera");
+
+      camera.addComponent("camera", {
+        clearColor: new Color(0.1, 0.1, 0.1)
+      });
+
+      camera.setPosition(0, 0, 3);
+
+      app.root.addChild(camera);
+
+      // setup initial light
+      const light = new Entity("light");
+
+      light.addComponent("light");
+
+      light.setEulerAngles(45, 0, 0);
+
+      app.root.addChild(light);
+
+      // setup initial cube
+      const cube = new Entity("cube");
+
+      cube.addComponent("model", {
+        type: "cube"
+      });
+
+      app.root.addChild(cube);
+
+      app.on("update", deltaTime => {
+        cube.rotate(10 * deltaTime, 20 * deltaTime, 30 * deltaTime);
+      });
+
+      // start app
+      app.start();
+    }
+  },
+  mounted() {
+    this.init();
+  }
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/pages/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -13,11 +13,13 @@ export const routes = [
   {
     path: "/scanner",
     name: "Scanner",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Scanner.vue")
+      import(/* webpackChunkName: "scanner" */ "@/pages/Scanner.vue")
+  },
+  {
+    path: "/3d",
+    name: "3D",
+    component: () => import(/* webpackChunkName: "3d" */ "@/pages/3D.vue")
   }
 ];
 
