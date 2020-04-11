58b560e67af84ed8c19d765c372df690a5fa6f80
Daniel LaCosse <D@nielLaCos.se>
2020-04-11 19:02:05 -0400

    add ALL the barcodes

--- src/pages/Scanner.vue
+++ src/pages/Scanner.vue
@@ -24,7 +24,19 @@ export default {
             target: this.$refs.scanner
           },
           decoder: {
-            readers: ["i2of5_reader"]
+            readers: [
+              "i2of5_reader",
+              "code_128_reader",
+              "ean_reader",
+              "ean_8_reader",
+              "code_39_reader",
+              "code_39_vin_reader",
+              "codabar_reader",
+              "upc_reader",
+              "upc_e_reader",
+              "2of5_reader",
+              "code_93_reader"
+            ]
           }
         },
         error => {
