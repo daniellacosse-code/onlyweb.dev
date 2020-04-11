00bc9285c0a30b47e592457a9dd9c56be8dbe178
Daniel LaCosse <D@nielLaCos.se>
2020-04-11 11:00:43 -0400

    install rustup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# install & compile rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 wasm-pack build
+
 yarn install
