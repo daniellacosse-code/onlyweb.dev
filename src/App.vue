0fd9eab57baaa1cb8097894d54399929d211e2d6
Daniel LaCosse <D@nielLaCos.se>
2020-04-11 18:31:32 -0400

    do it the recommended way

--- src/App.vue
+++ src/App.vue
@@ -45,8 +45,62 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~bulma/sass/utilities/_all";
+
+$primary: #42b983;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099ff;
+$twitter-invert: findColorInvert($twitter);
+
+$colors: (
+  "white": (
+    $white,
+    $black
+  ),
+  "black": (
+    $black,
+    $white
+  ),
+  "light": (
+    $light,
+    $light-invert
+  ),
+  "dark": (
+    $dark,
+    $dark-invert
+  ),
+  "primary": (
+    $primary,
+    $primary-invert
+  ),
+  "info": (
+    $info,
+    $info-invert
+  ),
+  "success": (
+    $success,
+    $success-invert
+  ),
+  "warning": (
+    $warning,
+    $warning-invert
+  ),
+  "danger": (
+    $danger,
+    $danger-invert
+  ),
+  "twitter": (
+    $twitter,
+    $twitter-invert
+  )
+);
+
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
 @import "~bulma";
-@import "~buefy/src/scss/components/_loading.scss";
+@import "~buefy/src/scss/buefy";
 
 html {
   background-color: $primary;
