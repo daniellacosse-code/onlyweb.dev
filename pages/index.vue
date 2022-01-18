4ab7f06be1a4fbe16318be8d11bac98a301ece8f
daniellacosse <d@niellacos.se>
2022-01-17 20:45:14 -0500

    ok lets see

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <img class="Logo" src="/only/icon.png" alt="logo" />
+    <img class="Logo" src="~/static/icon.png" alt="logo" />
 
     <h1>only web</h1>
     <h2>A modern native web demo, written in Vue.js</h2>
@@ -15,7 +15,7 @@
       for a guide on how to install this app on your device.
     </h3>
 
-    <img class="Diagram" src="/only/architecture.png" alt="architecture" />
+    <img class="Diagram" src="~/static/architecture.png" alt="architecture" />
     <figcaption>
       Source:
       <a href="https://github.com/daniellacosse/only-web">
