570d7be84156daf9aca7c500c0a7b20bbe725bae
daniellacosse <d@niellacos.se>
2022-01-30 13:47:53 -0500

    only one root

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,4 +33,5 @@ const developmentConfig = () => ({
   }
 });
 
+// ISSUE: #50 dependencies: nuxt3, vue3, vite, and the old insecure packages
 export default process.env.VERCEL ? productionConfig : developmentConfig();
