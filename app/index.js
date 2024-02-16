905392ffb1178f332383a6fd88b2d60fc9021f2a
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-02-16 18:45:24 -0500

    try importing stuff to see if deno's like OHHHH ok

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@
 import { resolve } from "https://deno.land/std@0.216.0/path/mod.ts";
 import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
 
+import "./pages/index.js";
+
 /**
  * @param {string} path
  * @returns {Promise<Uint8Array>} the file contents at the path
