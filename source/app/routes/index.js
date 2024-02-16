1aa0f55f35c4c4bf7f91fee8f6603725e699c969
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-02-15 23:43:45 -0500

    try this

--- source/app/routes/index.js
+++ source/app/routes/index.js
@@ -1,43 +1,47 @@
-import Response from "../../framework/backend/main.js";
+// import Response from "../../framework/backend/main.js";
 
-const pageStyle = Response.html`
-  <style>
-    main {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 1em;
-      height: 100svh;
-    }
-    h1 {
-      font-family: sans-serif;
-      font-size: 2em;
-      font-weight: bold;
-      color: blue;
-    }
+// const pageStyle = Response.html`
+//   <style>
+//     main {
+//       display: flex;
+//       flex-direction: column;
+//       justify-content: center;
+//       align-items: center;
+//       gap: 1em;
+//       height: 100svh;
+//     }
+//     h1 {
+//       font-family: sans-serif;
+//       font-size: 2em;
+//       font-weight: bold;
+//       color: blue;
+//     }
 
-    a {
-      font-family: sans-serif;
-      font-size: 1.5em;
-      color: orange;
-      text-decoration: underline;
-    }
-  </style>
-`;
+//     a {
+//       font-family: sans-serif;
+//       font-size: 1.5em;
+//       color: orange;
+//       text-decoration: underline;
+//     }
+//   </style>
+// `;
 
-const pageMessage = "Hello, World! <script>alert('XSS');</script>";
+// const pageMessage = "Hello, World! <script>alert('XSS');</script>";
 
-export default () =>
-  Response.html`<html>
-    <head>
-      <title>My Cool App</title>
-      ${pageStyle}
-    </head>
-    <body>
-      <main>
-        <h1>${pageMessage}</h1>
-        <a href="/api/app/routes/demo/image">view image</a>
-      </main>
-    </body>
-  </html>`;
+// export default () =>
+//   Response.html`<html>
+//     <head>
+//       <title>My Cool App</title>
+//       ${pageStyle}
+//     </head>
+//     <body>
+//       <main>
+//         <h1>${pageMessage}</h1>
+//         <a href="/api/app/routes/demo/image">view image</a>
+//       </main>
+//     </body>
+//   </html>`;
+function handler() {
+  return new Response("Hello, World!");
+}
+Deno.serve(handler);
