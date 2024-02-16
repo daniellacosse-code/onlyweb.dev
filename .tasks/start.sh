e6027628339a66658bfe2300009a4ee44dbbd0ae
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-02-16 18:34:07 -0500

    I think this should work?

--- .tasks/start.sh
+++ .tasks/start.sh
@@ -3,6 +3,7 @@
 while :
   do
     clear
+    echo "Starting server..."
     deno run --allow-net --allow-read=. app/index.js
     sleep 2
   done
--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 // @ts-check
 
-import * as response from "../framework/backend/response.js";
+import { resolve } from "https://deno.land/std@0.216.0/path/mod.ts";
+import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
 
 /**
  * @param {string} path
  * @returns {Promise<Uint8Array>} the file contents at the path
  *                                relative to the app/index.js file
  */
-export const readRelativeFilepath = (path) =>
-  Deno.readFile(new URL(path, import.meta.url));
+export const readRootFilepath = (path) =>
+  Deno.readFile(resolve(Deno.cwd(), path));
 
 Deno.serve(async (request) => {
   const requestURL = new URL(request.url);
@@ -21,19 +22,13 @@ Deno.serve(async (request) => {
 
   switch (requestPath.split("/").at(1)) {
     case "favicon.ico":
-      return response.file(
-        await readRelativeFilepath("./assets/images/logo.png")
+      return serveFile(
+        request,
+        resolve(Deno.cwd(), "./assets/images/logo.png")
       );
-    case "assets":
-      return response.file(await readRelativeFilepath(`.${requestPath}`));
-    case "elements":
-      return response.js(
-        new TextDecoder().decode(await readRelativeFilepath(`.${requestPath}`))
-      );
+    case "app":
     case "framework":
-      return response.js(
-        new TextDecoder().decode(await readRelativeFilepath(`..${requestPath}`))
-      );
+      return serveFile(request, resolve(Deno.cwd(), `.${requestPath}`));
     case "pages":
       return (await import(`.${requestPath}`)).default(request);
     default: {
