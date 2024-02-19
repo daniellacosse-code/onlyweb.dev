4a0cc53c0bc83cfe3a93bc26314a9e82605de4b3
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-02-19 12:07:41 -0500

    replace minify

--- framework/backend/html/inline.js
+++ framework/backend/html/inline.js
@@ -1,11 +1,11 @@
-import { htmlMinify } from "/framework/shared/html/minify.js";
+import { minify } from "/framework/shared/html/minify.js";
 
 import { response } from "./response.js";
 import { encode } from "https://deno.land/std@v0.56.0/encoding/base64.ts";
 
 export function inline(filePath, host = "http://localhost:8000") {
   const fileContents = Deno.readTextFileSync(filePath);
-  const sanitizedScript = htmlMinify(fileContents).replaceAll(
+  const sanitizedScript = minify(fileContents).replaceAll(
     ' from "/',
     ` from "${host}/`
   );
