711560be3c2b907538432d6d46d5fa195317d284
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-02-20 07:57:40 -0500

    fix load error

--- framework/backend/components/register-inline.js
+++ framework/backend/components/register-inline.js
@@ -1,7 +1,7 @@
 import { encode } from "https://deno.land/std@v0.56.0/encoding/base64.ts";
 
 import { minify } from "/framework/shared/html/minify.js";
-import { response } from "../pages/html.js";
+import * as pages from "../pages/html.js";
 
 // register components inline ONLY when you're sure all
 // their dependencies are also loaded
@@ -11,7 +11,8 @@ export function registerInline(filePath, host = "http://localhost:8000") {
     .replaceAll(' from "/', ` from "${host}/`)
     .replaceAll('import "/', `import "${host}/`);
 
-  return response`<script type="module" src="data:application/javascript;base64,${encode(
-    sanitizedScript
-  )}"></script>`;
+  return pages.html`<script
+    type="module"
+    src="data:application/javascript;base64,${encode(sanitizedScript)}"
+  ></script>`;
 }
