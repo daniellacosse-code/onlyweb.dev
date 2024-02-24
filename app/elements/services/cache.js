2e57962706ae8c98e51b3d64db738628b3d40630
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-02-24 06:10:50 -0500

    TODO: cache service

---
+++ app/elements/services/cache.js
@@ -0,0 +1,38 @@
+// RegisterWorker({
+//   tag,
+//   source: "/app/components/workers/cache.js"
+// });
+
+// export default () => ({
+//   state: { tag },
+//   handleMount: async () => {
+//     const cache = await caches.open(this.state.tag);
+
+//     // await cache.delete();
+
+//     return cache.addAll([
+//       "/framework/shared/cuid.js",
+//       "/framework/shared/handle-template.js",
+//       "/framework/shared/user-agent.js",
+//       "/framework/shared/html/escape.js",
+//       "/framework/shared/html/minify.js",
+//       "/framework/frontend/element/html.js",
+//       "/framework/frontend/element/register.js",
+//       "/framework/frontend/store/register.js",
+//       "/app/assets/images/logo.png",
+//       "/app/elements/core/button.js",
+//       "/app/elements/core/loading/skeleton.js",
+//       "/app/elements/keycdn/image.js"
+//     ]);
+//   },
+//   handleEvent: async (event) => {
+//     if (!event.request) return null;
+
+//     const cache = await caches.open(this.context.tag);
+//     const response = await cache.match(event.request);
+
+//     if (!response) return null;
+
+//     return response;
+//   }
+// });
