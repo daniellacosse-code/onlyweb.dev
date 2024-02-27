0d30cfeade8952862ada6571742ab634bdc43bd8
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-02-26 22:44:03 -0500

    closer

--- app/elements/core/input.js
+++ app/elements/core/input.js
@@ -2,59 +2,84 @@ import FrontendElement from "/framework/frontend-element/entry.js";
 
 FrontendElement.Register("core-input", {
   attributes: {
-    label: String,
-    ["has-value"]: Boolean
+    label: String
   },
   handleMount() {
-    this.attributes.contenteditable = true;
-    this.attributes["has-value"] = Boolean(this.textContent);
+    // re-rendering messes with the focus, so we
+    // manage the state this way
+    this.addEventListener("input", (event) => {
+      this.hasContent = Boolean(event.target.textContent);
 
-    this.addEventListener("input", () => {
-      this.attributes["has-value"] = Boolean(this.textContent);
+      if (this.hasContent) {
+        this.querySelector("label").classList.add("hidden");
+      } else {
+        this.querySelector("label").classList.remove("hidden");
+      }
     });
+
+    this.attributes.tabindex = 0;
+    this.addEventListener("focus", () => this.querySelector("#input").focus());
   },
   handleRender({ label = "" }) {
     return FrontendElement.html`<style>
-      :host, label {
-        box-sizing: border-box;
-        display: inline-block;
-        width: 100%;
-        min-height: 100%;
-      }
-      :host {
-        position: relative;
-        border: 3px solid var(--color-foreground);
-        border-radius: var(--size-narrow);
-        color: var(--color-foreground);
-        transition: border-color 140ms var(--animation-timing-function);
-        background: transparent;
-        padding: var(--size-narrow);
-        outline: 0;
-      }
-      label {
-        position: absolute;
-        top: 0;
-        left: 0;
-        color: var(--color-neutral);
-        padding: var(--size-narrow);
-        opacity: 1;
-        transition: opacity 140ms var(--animation-timing-function);
-        z-index: -1;
-      }
-      label.hidden,
-      :host(:focus) label {
-        opacity: 0;
-      }
-      :host(:focus),
-      :host(:active) {
-        border-color: var(--color-highlight);
-      }
-      ::selection {
-        background-color: var(--color-highlight);
-        color: var(--color-background);
-      }
-    </style>
-    <label class="${this.attributes["has-value"] && "hidden"}">${label}</label>
-    <slot></slot>`;
+        ::selection {
+          background-color: var(--color-highlight);
+          color: var(--color-background);
+        }
+        :host,
+        label,
+        #input {
+          box-sizing: border-box;
+          display: inline-block;
+          min-height: 100%;
+          width: 100%;
+        }
+        :host,
+        #input {
+          outline: 0;
+        }
+        :host,
+        label {
+          padding: var(--size-narrow);
+        }
+        label.hidden,
+        :host(:focus) > label {
+          opacity: 0;
+        }
+        :host {
+          border-radius: var(--size-narrow);
+          border: 3px solid var(--color-foreground);
+          position: relative;
+          transition: border-color 140ms var(--animation-timing-function);
+        }
+        :host(:focus-within),
+        :host(:active) {
+          border-color: var(--color-highlight);
+        }
+        #input, b, i, u {
+          color: var(--color-foreground);
+        }
+        b {
+          font-weight: bold;
+        }
+        i {
+          font-style: italic;
+        }
+        u {
+          text-decoration: underline;
+        }
+        label {
+          color: var(--color-neutral);
+          left: 0;
+          opacity: 1;
+          position: absolute;
+          top: 0;
+          transition: opacity 140ms var(--animation-timing-function);
+          z-index: -1;
+        }
+
+      </style>
+      <label for="input">${label}</label>
+      <div contenteditable="true" role="input" id="input"></div>`;
   }
 });
