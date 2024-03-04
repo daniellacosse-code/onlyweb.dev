ebd080b16e14164c612c7c6d2d04f642905c817e
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-03-04 08:39:51 +0530

    fix sw

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import Backend from "/framework/backend/module.js";
 
-import "/app/pages/main.js";
 import "/app/pages/favicon.js";
+import "/app/pages/root.js";
 import "/app/pages/__gallery__.js";
 
 Backend.start();
--- app/pages/root.js
+++ app/pages/root.js
@@ -144,15 +144,17 @@ Backend.Page.Register(route, {
       );
     });
 
-    event.respondWith(caches.open("${route}").then((cache) => {
-      return cache.match(event.request).then((cachedResponse) => {
-        const fetchedResponse = fetch(event.request).then((networkResponse) => {
-          cache.put(event.request, networkResponse.clone());
+    self.addEventListener("fetch", (event) => {
+      event.respondWith(caches.open("${route}").then((cache) => {
+        return cache.match(event.request).then((cachedResponse) => {
+          const fetchedResponse = fetch(event.request).then((networkResponse) => {
+            cache.put(event.request, networkResponse.clone());
 
-          return networkResponse;
+            return networkResponse;
+          });
+
+          return cachedResponse || fetchedResponse;
         });
-
-        return cachedResponse || fetchedResponse;
-      });
-    }));`
+      }));
+    });`
 });
