3cbd111ae0fe7bada6d692ee9ace6b6fd888b8f0
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-03-10 11:52:39 -0400

    log test and docs

--- framework/shared/log.js
+++ framework/shared/log.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const MAX_MESSAGE_LENGTH = 128;
 const MAX_DETAIL_LENGTH = 256;
 
@@ -10,20 +12,38 @@ const LOG_LEVELS = {
   critical: 5
 };
 
-const Log = ({ message, detail, level = "info" }) => {
+/**
+ * Log a message to the console
+ * @param {object} options
+ * @param {string} options.message the message to log
+ * @param {object} [options.detail] additional data to log
+ * @param {("debug" | "info" | "warn" | "error")} [options.level] one of "debug", "info", "warn", "error"
+ * @param {typeof console} [options._console] for testing
+ * @returns {void} Nothing: logs to console
+ */
+const Log = ({ message, detail, level = "info", _console = console }) => {
+  /** @type {{ o: string, l: number, m: string, d?: string }} */
   const logObject = {
-    m: message.slice(0, MAX_MESSAGE_LENGTH),
     o: new Date().toISOString(),
-    l: LOG_LEVELS[level]
+    l: LOG_LEVELS[level],
+    m: message.trim().slice(0, MAX_MESSAGE_LENGTH)
   };
 
   if (detail) {
     logObject.d = JSON.stringify(detail).slice(0, MAX_DETAIL_LENGTH);
   }
 
-  console[level](logObject);
+  _console[level](JSON.stringify(logObject));
 };
 
+/**
+ * Logs an error to the console
+ * @param {Error} error the error to log
+ * @returns {void} Nothing: logs to console
+ * @example
+ * LogError(new Error("Something went wrong"));
+ * // {"o":"2020-01-01T00:00:00.000Z","l":4,"m":"Something went wrong"}
+ */
 export const LogError = (error) =>
   Log({ message: error.message, detail: error, level: "error" });
 export default Log;
