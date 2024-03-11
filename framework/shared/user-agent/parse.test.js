2210f107deff5bce4d98021329945128057e7cd8
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-03-11 05:20:09 -0400

    add testing check

--- framework/shared/user-agent/parse.test.js
+++ framework/shared/user-agent/parse.test.js
@@ -8,11 +8,11 @@ Deno.test("parse", () => {
       "Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36"
     ),
     {
-      browser: {
+      engine: {
         name: "Chrome",
         version: 88
       },
-      engine: {
+      renderer: {
         name: "WebKit",
         version: 537.36
       }
