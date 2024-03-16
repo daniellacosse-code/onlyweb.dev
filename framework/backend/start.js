34cd1da8adfb84345e23731833e8f12bacd6c8e3
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-03-16 16:22:08 +0000

    default object

--- framework/backend/start.js
+++ framework/backend/start.js
@@ -1,7 +1,7 @@
 import { resolve } from "https://deno.land/std@0.216.0/path/mod.ts";
 import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
 
-export default ({ port = 8080 }) =>
+export default ({ port = 8080 } = {}) =>
   Deno.serve({ port }, (request) => {
     const requestURL = new URL(request.url);
     const requestPath = requestURL.pathname.startsWith("/")
