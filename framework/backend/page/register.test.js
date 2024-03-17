d9e6396a8eb86843e609710d054351c466716502
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2024-03-17 00:16:19 -0400

    attempting 80% "smoke" test coverage - got to 76% (#187)

--- framework/backend/page/register.test.js
+++ framework/backend/page/register.test.js
@@ -2,7 +2,22 @@ import { assertEquals } from "https://deno.land/std@0.219.0/assert/assert_equals
 import register from "/framework/backend/page/register.js";
 
 Deno.test("register", async () => {
-  await register("/", () => "test");
+  await register("/", {
+    responses: {
+      handleDefault: () => new Response(),
+      handleServiceWorker: () => new Response()
+    }
+  });
 
   assertEquals(Boolean(globalThis.customPages.get("/")), true);
+  assertEquals(
+    Boolean(globalThis.customPages.get("/")(new Request("localhost:8000"))),
+    true
+  );
+  assertEquals(
+    Boolean(
+      globalThis.customPages.get("/")(new Request("localhost:8000/?service"))
+    ),
+    true
+  );
 });
--- framework/frontend/element/register.test.js
+++ framework/frontend/element/register.test.js
@@ -1,6 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.219.0/assert/assert_equals.ts";
 import register from "/framework/frontend/element/register.js";
 
+// HTML Element mocks
 globalThis.customElements = {
   define(tag, element) {
     globalThis.customElements[tag] = element;
@@ -10,10 +11,31 @@ globalThis.customElements = {
   }
 };
 
-globalThis.HTMLElement = class HTMLElement {};
+globalThis.HTMLElement = class HTMLElement {
+  attachShadow() {
+    return {};
+  }
 
-Deno.test("register", async () => {
+  addEventListener() {}
+};
+
+Deno.test("element - register", async () => {
   await register("my-element", {});
 
   assertEquals(Boolean(globalThis.customElements.get("my-element")), true);
+  assertEquals(
+    new globalThis.customElements["my-element"]() instanceof HTMLElement,
+    true
+  );
+});
+
+Deno.test("element - addEventListener", async () => {
+  await register("my-element", {});
+
+  const element = new globalThis.customElements["my-element"]();
+
+  assertEquals(
+    element.addEventListener("click", () => {}),
+    undefined
+  );
 });
